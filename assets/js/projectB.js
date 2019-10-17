a49c0c165c4d9ae66f33345649d5b9c4d4534ba3
maxken1301 <56638440+maxken1301@users.noreply.github.com>
2019-10-18 03:03:17 +0800

    Update projectB.js

--- assets/js/projectB.js
+++ assets/js/projectB.js
@@ -1,3 +1,46 @@
-var message = "Hi Maxine!";
+var image = document.getElementById("imgB");
+var butt = document.getElementById("butt");
+var butt02 = document.getElementById("butt02");
+butt.addEventListener('click',function xx(){
+	hidestuff("messageBox");
+
+document.getElementById("message").textContent = "";
+$(image).css("max-width", "700px");
+$(image).css("max-height", "900px");
+image.src = "img/final/punch.gif";
+setTimeout(de, 1000);
+setTimeout(cImg, 2000);
+
+})
+function de(){
+	var audio = new Audio('balalala.mp3');
+audio.play();
+}
+
+
+
+
+
+function cImg(){
+image.src = "img/final.gif";
+showstuff("messageBox");
+}
+
+function showstuff(boxid){
+   document.getElementById(boxid).style.visibility="visible";
+}
+ 
+function hidestuff(boxid){
+   document.getElementById(boxid).style.visibility="hidden";
+}
+function wait(ms)
+{
+var d = new Date();
+var d2 = null;
+do { d2 = new Date(); }
+while(d2-d < ms);
+}
+
+
 
 
